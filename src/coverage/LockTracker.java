7c6216a2ed9fcc91050fc5715610be298e5c97f5
jordan howorth <skb15165@uni.strath.ac.uk>
2018-03-01 15:41:06 +0000

    minor fixes to locktest

--- src/coverage/LockTracker.java
+++ src/coverage/LockTracker.java
@@ -4,56 +4,66 @@ import java.util.ArrayList;
 
 public class LockTracker {
 
-    public enum Locks {
-        GetBalance, SetBalance, Withdraw, Deposit, Transfer, NoLock
-    }
+   private Locks currentLocks;
+    private static ArrayList<Locks[]> lockList;
 
-    private Locks currentLocks;
-    private ArrayList<Locks[]> lockList;
-
-    public LockTracker(){
+    public LockTracker() {
         currentLocks = Locks.NoLock;
         lockList = new ArrayList<>();
+ }
 
-    }
-
-    public void setCurrentLocks(Locks newLocks){
-        if(currentLocks.equals(Locks.NoLock)){
+    public void setCurrentLocks(Locks newLocks) {
+        if (currentLocks.equals(Locks.NoLock)) {
             currentLocks = newLocks;
-        }else{
-            addLockPair(currentLocks,newLocks);
+        } else {
+            addLockPair(currentLocks, newLocks);
             currentLocks = Locks.NoLock;
         }
     }
 
+    public void addLockPair(Locks current, Locks newLock) {
+printResults();
 
-    public void addLockPair(Locks current, Locks newLock){
+        //Check if we already have the pair (current, newLock) or (newLock, current). Add them if we do not.
+        for (Locks[] lock : lockList) {
+            if ((lock[0].equals(current) && lock[1].equals(newLock)) || (lock[1].equals(current) && lock[0].equals(newLock))) {
 
+                System.out.println("Naughty: " +  current + " " +newLock);
+                return;
 
-        for(Locks[] lock : lockList){
-            if((lock[0].equals(current) && lock[1].equals(newLock)) || (lock[1].equals(current) && lock[0].equals(newLock))){
-                // only do something if they do not match, if they match you dont want to add them again
-            }
-            else{
-                Locks[] newPair = new Locks[2];
-                newPair[0] = current;
-                newPair[1] = newLock;
-                lockList.add(newPair);
             }
         }
+
+        System.out.println(current + " " +newLock);
+        Locks[] newPair = new Locks[2];
+        newPair[0] = current;
+        newPair[1] = newLock;
+
+        lockList.add(newPair);
+
+
+
     }
 
-    public int getLocksListSize(){
+    public int getLocksListSize() {
         return lockList.size();
     }
 
-    public void printResults(){
+    public void printResults() {
         System.out.println(" ----------------------------");
         System.out.println("Lock Pairs are as follows: ");
 
-        for(Locks[] locks: lockList){
+
+        for (Locks[] locks : lockList) {
             System.out.println(locks[0] + " & " + locks[1]);
         }
+
+
+        System.out.println("--------------------------");
+    }
+
+    public enum Locks {
+        GetBalance, SetBalance, Withdraw, Deposit, Transfer, NoLock
     }
 
 
